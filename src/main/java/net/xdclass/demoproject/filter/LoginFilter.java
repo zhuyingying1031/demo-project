c6cbcd19712efa1dd17d3831741477a28f568dda
sy.zhou <461423775@qq.com>
2020-09-22 18:20:31 +0800

    自定义Filter未登录json错误码提示

--- src/main/java/net/xdclass/demoproject/filter/LoginFilter.java
+++ src/main/java/net/xdclass/demoproject/filter/LoginFilter.java
@@ -1,7 +1,9 @@
 package net.xdclass.demoproject.filter;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import net.xdclass.demoproject.domain.User;
 import net.xdclass.demoproject.service.impl.UserServiceImpl;
+import net.xdclass.demoproject.utils.JsonData;
 import org.springframework.util.StringUtils;
 
 import javax.servlet.*;
@@ -9,9 +11,13 @@ import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebFilter(urlPatterns = "/api/v1/pri/*",filterName = "loginFilter")
 public class LoginFilter implements Filter {
+
+	private  static final ObjectMapper objectMapper = new ObjectMapper();
+
 	@Override
 	public void init(FilterConfig filterConfig) throws ServletException {
 
@@ -27,18 +33,35 @@ public class LoginFilter implements Filter {
 		if (StringUtils.isEmpty(token)){
 			token=req.getParameter("token");
 		}
-		if (StringUtils.isEmpty(token)){
-			return;
-		}else {
+		if (!StringUtils.isEmpty(token)){
 			User user=UserServiceImpl.sessionMap.get(token);
 			if (user!=null){
 
 				filterChain.doFilter(servletRequest,servletResponse);
+			}else {
+				JsonData jsonData=JsonData.buildError("登录失败，无效token",-2);
+				String jsonStr=objectMapper.writeValueAsString(jsonData);
+				renderJson(resp,jsonStr);
 			}
+		}else {
+			JsonData jsonData=JsonData.buildError("未登录",-3);
+			String jsonStr=objectMapper.writeValueAsString(jsonData);
+			renderJson(resp,jsonStr);
 
 		}
 
 	}
+
+	private void renderJson(HttpServletResponse response,String json){
+		response.setCharacterEncoding("utf-8");
+		response.setContentType("application/json");
+		try(PrintWriter writer=response.getWriter()){
+			writer.print(json);
+		}catch (Exception e)
+		{ e.printStackTrace(); }
+	}
+
+
 
 	@Override
 	public void destroy() {
